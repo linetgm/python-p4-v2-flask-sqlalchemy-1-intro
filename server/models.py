3cb3efed357f77a55c42bc7b342ec3236018e68d
Linet <muthiigathoni@gmail.com>
2024-07-17 23:24:44 +0300

    complete

--- server/models.py
+++ server/models.py
@@ -8,6 +8,12 @@ metadata = MetaData()
 db = SQLAlchemy(metadata=metadata)
 
 # define a model class by inheriting from db.Model.
+class Pet(db.Model):
+    __tablename__ = 'pets'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    species = db.Column(db.String)
 
 
 class Pet(db.Model):
